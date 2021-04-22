a2a47084821bcfe98ab4b796420f611262ae0e3f
pancitoConQuesito <gonzalo.astorga.@gmail.com>
2021-04-22 04:42:09 -0400

    a

---
+++ js/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.marco'),{delay:300};
+ScrollReveal().reveal('.footer'),{delay:300};
+ScrollReveal().reveal('#archivo'),{delay:300};
+ScrollReveal().reveal('#archivo img'),{delay:300};
+ScrollReveal().reveal('.marco img'),{delay:300};
